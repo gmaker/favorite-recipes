1d62dae7f0ce0db3d89f3ff8138407c32efffcfb
gmaker <mr.gmaker@yandex.ru>
2025-03-05 22:38:36 +0300

    minor changes

--- backend-api/src/main/java/art/gmaker/backend_api_recipes/controller/MediaController.java
+++ backend-api/src/main/java/art/gmaker/backend_api_recipes/controller/MediaController.java
@@ -16,21 +16,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class MediaController {
 
     private final MediaService mediaService;
-    private final String mediaBaseUrl;
 
-    public MediaController(MediaService mediaService,
-                           @Value("${media.base.url:http://localhost:8081}") String mediaBaseUrl) {
+    public MediaController(MediaService mediaService) {
         this.mediaService = mediaService;
-        this.mediaBaseUrl = mediaBaseUrl;
     }
     // Endpoint для загрузки медиа с обязательным параметром postId
     @PostMapping("/upload")
     public ResponseEntity<MediaResponse> uploadMedia(@RequestParam("file") MultipartFile file,
                                                      @RequestParam("postId") long postId) {
-        // Сохраняем файл. Метод возвращает относительный путь, например, "/uploads/1740714192172_1740714386168_00.png"
         String relativePath = mediaService.saveMedia(file, postId);
-        // Формируем абсолютный URL для клиента, используя базовый URL, который можно задать в конфигурации
-        String absoluteUrl = mediaBaseUrl + relativePath;
-        return ResponseEntity.ok(new MediaResponse(absoluteUrl));
+        return ResponseEntity.ok(new MediaResponse(relativePath));
     }
 }
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,17 +4,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/uploads': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        // если нужно, можно настроить pathRewrite
-        // pathRewrite: { '^/uploads': '/uploads' },
-      },
       '/api': {
         target: 'http://localhost:8081',
         changeOrigin: true,
-        // если нужно, можно настроить pathRewrite
-        // pathRewrite: { '^/uploads': '/uploads' },
+      },
+      '/uploads': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
       },
     },
   },
